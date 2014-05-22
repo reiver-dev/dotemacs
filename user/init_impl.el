8205145dcdbc14c06f3ec55d4ff85ee6ed7bd390
reiver <hlam-box@yandex.ru>
2014-05-22 22:18:48 +0400

    Removed el-get

--- user/init_impl.el
+++ user/init_impl.el
@@ -8,8 +8,6 @@
 
 (defconst my:backup-dir (concat user-emacs-directory-full "backup/"))
 (defconst my:autosave-dir (concat user-emacs-directory-full "autosave/"))
-(defconst my:recipes-dir (concat my:user-dir "el-get-recipes/"))
-(defconst my:el-get-dir (concat user-emacs-directory-full "/el-get/el-get/"))
 (defconst my:snippets-dir (concat my:user-dir "snippets/"))
 
 (dolist (dir (list my:backup-dir my:autosave-dir my:modules-dir my:snippets-dir))
@@ -191,169 +189,6 @@
 (global-set-key (kbd "C-x C-k") 'my:kill-and-close-current)
 
 ;; ### PACKAGES ### ;;
-
-(add-to-list 'load-path my:el-get-dir)
-
-(defun my:packages-el-get ()
-  (interactive)
-  (let* ((url "https://github.com/dimitri/el-get")
-         (res (call-process "git" nil (get-buffer-create "*el-get-download*") t
-                            "clone" url my:el-get-dir)))
-    (if (zerop res)
-        (progn
-          (load-library "el-get")
-          (el-get-post-install "el-get"))
-      (message "Failed to clone el-get"))))
-
-(when (require 'el-get nil t)
-  (setq el-get-verbose t)
-  (add-to-list 'el-get-recipe-path my:recipes-dir)
-  (setq el-get-sources
-        '((:name el-get
-                 :branch "master")
-          ;; Navigation, editing, appearance
-          (:name smooth-scrolling
-                 :after (setq smooth-scroll-margin 4))
-          (:name linum-relative
-                 :after (progn (setq-default linum-relative-format "%4s ")))
-          (:name undo-tree
-                 :before (progn
-                           (setq undo-tree-mode-lighter ""))
-                 :after (progn
-                          (global-undo-tree-mode)))
-          (:name smart-mode-line
-                 :before (progn (setq sml/theme 'dark))
-                 :after (progn (sml/setup)))
-          (:name buffer-move
-                 :after (progn
-                          (global-set-key (kbd "<C-S-up>") 'buf-move-up)
-                          (global-set-key (kbd "<C-S-down>") 'buf-move-down)
-                          (global-set-key (kbd "<C-S-left>") 'buf-move-left)
-                          (global-set-key (kbd "<C-S-right>") 'buf-move-right)))
-          (:name multiple-cursors
-                 :after (progn
-                          (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-                          (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-                          (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)))
-          (:name expand-region
-                 :after (progn
-                          (global-set-key (kbd "C-t") 'er/expand-region)
-                          (global-set-key (kbd "C-S-t") 'er/contract-region)))
-          (:name smartparens
-                 :after (progn
-                          (setq sp-show-pair-from-inside t)
-                          (smartparens-global-mode t)
-                          (show-smartparens-global-mode t)))
-          (:name ace-jump-mode
-                 :features "ace-jump-mode")
-          ;; Completion
-          (:name auto-complete
-                 :features auto-complete-config
-                 :after (progn
-                          (setq ac-use-menu-map t)
-                          (define-key ac-menu-map (kbd "C-p") 'ac-previous)
-                          (define-key ac-menu-map (kbd "C-n") 'ac-next)
-                          (ac-config-default)
-                          (ac-linum-workaround)))
-          (:name yasnippet
-                 :features yasnippet
-                 :after (progn 
-                          (add-to-list 'yas-snippet-dirs my:snippets-dir)
-                          (define-key yas-minor-mode-map (kbd "<tab>") nil)
-                          (define-key yas-minor-mode-map (kbd "TAB") nil)
-                          (yas-global-mode t)
-                          (add-to-list 'hippie-expand-try-functions-list
-                                       'yas-hippie-try-expand)))
-          ;; Fast access and searching
-          (:name flx
-                 :after (progn
-                          (require 'flx-ido)
-                          (flx-ido-mode 1)
-                          (setq ido-use-faces nil)))
-          (:name ido-ubiquitous
-                 :after (ido-ubiquitous))
-          (:name ido-vertical-mode
-                 :after (ido-vertical-mode 1))
-          (:name smex
-                 :after (progn 
-                          (global-set-key (kbd "M-x") 'smex)
-                          (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
-          (:name ag
-                 :after (progn (setq ag-highlight-search t)))
-          ;; Project management and project tree
-          (:name projectile
-                 :after (progn
-                          (defadvice projectile-project-root (around projectile-my-root activate)
-                            (if (and my:project-root
-                                     (s-starts-with? my:project-root
-                                                     default-directory))
-                                (setq ad-return-value my:project-root)
-                              ad-do-it))
-                          (projectile-global-mode)))
-          ;; (:name perspective
-          ;;        :after (progn
-          ;;                 (require 'perspective)
-          ;;                 (persp-mode t)))
-          (:name project-explorer
-                 :after (progn
-                          (global-set-key (kbd "<f5>") 'project-explorer-open)))
-          (:name sr-speedbar
-                 :after (progn
-                          (setq speedbar-use-images nil)
-                          (global-set-key (kbd "<f7>") 'sr-speedbar-toggle)))
-          ;; Evil mode and Co
-          (:name evil
-                 :before (setq evil-want-C-u-scroll t
-                               evil-want-C-w-in-emacs-state t)
-                 :after (progn
-                          (setq evil-emacs-state-modes '(Custom-mode
-                                                         direx:direx-mode
-                                                         project-explorer-mode
-                                                         cider-repl-mode))
-                          (define-key evil-insert-state-map (kbd "C-SPC") 'auto-complete)
-                          (define-key evil-normal-state-map (kbd "SPC") 'evil-ace-jump-word-mode)
-                          (evil-define-state normal-im
-                            "Motion with input method for searching"
-                            :tag " <S> "
-                            :enable (motion)
-                            :input-method t)
-                          (defadvice evil-search-incrementally (around evil-search-f-method activate)
-                            (evil-normal-im-state) ad-do-it (evil-normal-state)) 
-                          (setq evil-search-module 'evil-search
-                                evil-want-C-u-scroll t
-                                evil-want-C-w-in-emacs-state t)
-                          (evil-mode 1)))
-          (:name evil-leader
-                 :after (progn
-                          (global-evil-leader-mode)))
-          ;; Language specific
-          (:name jedi)
-          (:name js2-mode
-                 :after (progn (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))))
-          (:name clojure-mode)
-          (:name cider)
-          (:name ac-nrepl
-                 :after (progn
-                          (add-hook 'cider-mode-hook 'ac-nrepl-setup)))))
-  (setq my:packages 
-        (mapcar 'el-get-as-symbol 
-                (mapcar 'el-get-source-name el-get-sources)))
-  (defun my:packages-sync () 
-    (interactive)
-    (el-get 'sync my:packages))
-  (defun my:packages-async () 
-    (interactive)
-    (el-get nil my:packages))
-  (defun my:packages-clean ()
-    (interactive)
-    (el-get-cleanup my:packages)))
-
-
-(when (require 'el-get nil t)
-  (el-get 'sync))
-
-
-               
 (defun my:package-initialize ()
   ;; Navigation, editing, appearance
   (use-package smooth-scrolling
@@ -364,10 +199,7 @@
     :config (progn (setq-default linum-relative-format "%4s ")))
   (use-package undo-tree
     :ensure t
-    :pre-init (progn
-                (setq undo-tree-mode-lighter ""))
-    :config (progn
-              (global-undo-tree-mode)))
+    :config (global-undo-tree-mode))
   (use-package smart-mode-line
     :ensure t
     :pre-init (progn (setq sml/theme 'dark))
@@ -447,11 +279,6 @@
                     (setq ad-return-value my:project-root)
                   ad-do-it))
               (projectile-global-mode)))
-  ;; (use-package perspective
-  ;; :ensure t
-  ;; :config (progn
-  ;;                 (require 'perspective)
-  ;;                 (persp-mode t)))
   (use-package project-explorer
     :ensure t
     :config (progn
@@ -473,6 +300,7 @@
                                              project-explorer-mode
                                              cider-repl-mode))
               (define-key evil-insert-state-map (kbd "C-SPC") 'auto-complete)
+              (define-key evil-insert-state-map (kbd "C-@") 'auto-complete)
               (define-key evil-normal-state-map (kbd "SPC") 'evil-ace-jump-word-mode)
               (evil-define-state normal-im
                 "Motion with input method for searching"
