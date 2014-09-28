d92e737c0f53c6b43bcfa2c3f9690c0b5bdb0679
Reiver <hlam-box@yandex.ru>
2014-09-28 19:44:29 +0400

    Helm added

--- user/init_impl.el
+++ user/init_impl.el
@@ -40,6 +40,7 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(tooltip-mode -1)
 
 ;; enable y/n answers
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -78,13 +79,14 @@
 ;; Text behavior
 (setq-default shift-select-mode nil
               truncate-lines nil
-              word-wrap t)
+              word-wrap t
+              sentence-end-double-space nil)
 
 ;; Indentation ;;
 (electric-indent-mode t)
 (setq-default indent-tabs-mode nil
               tab-width 4
-              fill-column 120) 
+              fill-column 120)
 
 
 ;; Spell Check ;;
@@ -96,6 +98,8 @@
                '("english" "[A-z]" "[^A-z]" "[']" nil ("-d" "en_GB") nil iso-8859-1))
   (setq ispell-program-name "hunspell"))
 
+(setq flyspell-issue-message-flag nil)
+
 ;; IDO mode ;;
 (ido-mode t)
 (recentf-mode t)
@@ -104,16 +108,30 @@
               ido-default-buffer-method 'selected-window
               ido-enable-last-directory-history nil
               ido-enable-flex-matching t)
+(add-to-list 'ido-ignore-buffers "^\\*helm")
 
 ;; Navigate windows with Shift-<arrows> ;;
 (windmove-default-keybindings)
 (setq-default windmove-wrap-around t)
+(winner-mode 1)
 
 ;; Comint ;;
 (setq-default comint-prompt-read-only t
               comint-scroll-to-bottom-on-input t)
 
 ;; Util ;;
+(setq my:bindings-mode-map (make-sparse-keymap))
+
+(define-minor-mode my:bindings-mode
+  "My settings"
+  :global t
+  :keymap my:bindings-mode-map)
+
+(defun my:kmap (key function)
+  (define-key my:bindings-mode-map (kbd (concat "C-; " key)) function)
+  (define-key my:bindings-mode-map (kbd (concat "C-c ; " key)) function))
+
+(my:bindings-mode 1)
 
 (defun my:minibuffer-set-key (key command)
   "Binds key to all common minibuffer states"
@@ -144,7 +162,8 @@ This is the same as using \\[set-mark-command] with the prefix argument."
   (interactive)
   (set-mark-command 1))
 
-(defadvice exchange-point-and-mark (before exchange-pnm-no-activate activate)
+(defadvice exchange-point-and-mark
+  (before exchange-pnm-no-activate activate compile)
   (ad-set-arg 0 t))
 
 (defun my:kill-line-to-indent ()
@@ -168,12 +187,12 @@ This is the same as using \\[set-mark-command] with the prefix argument."
   (transpose-lines 1)
   (indent-according-to-mode))
 
-;; Project root helpers 
+;; Project root helpers
 (defvar my:project-root nil)
 (defun my:project-root-set ()
   "Sets global project root to directory"
   (interactive)
-  (setq my:project-root 
+  (setq my:project-root
         (expand-file-name (ido-read-directory-name "Set project dir: "))))
 (defun my:project-root-unset ()
   "Resets global project root"
@@ -196,7 +215,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 (defun my:kill-current-buffer ()
   (interactive)
-  (kill-buffer (current-buffer))) 
+  (kill-buffer (current-buffer)))
 
 (defun my:kill-and-close-current ()
   (interactive)
@@ -205,49 +224,51 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
       (delete-window))
     (kill-buffer buf)))
 
-
 ;; Bindings ;;
 (global-unset-key (kbd "ESC ESC ESC"))
 (global-set-key (kbd "C-S-g") 'keyboard-escape-quit)
+(global-set-key (kbd "C-x C-g") 'keyboard-escape-quit)
 (global-set-key (kbd "<escape>") 'keyboard-quit)
 (my:minibuffer-set-key (kbd "<escape>") 'my:minibuffer-keyboard-quit)
 
 (global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
-(global-set-key (kbd "C-x C-c") 'ido-switch-buffer)
-(global-set-key (kbd "C-x B") 'ibuffer)
+(global-set-key (kbd "C-x C-c") 'switch-to-buffer)
 (global-set-key (kbd "C-c o") 'ff-find-other-file)
 
 (global-unset-key (kbd "M-t")) ;; which used to be transpose-words
 (global-set-key (kbd "M-t w") 'transpose-words)
+(global-set-key (kbd "M-t l") 'transpose-lines)
+(global-set-key (kbd "M-t u") 'my:move-line-up)
+(global-set-key (kbd "M-t d") 'my:move-line-down)
 (global-set-key (kbd "M-t s") 'transpose-sexps)
-(global-set-key (kbd "C-;") 'my:move-line-up)
-(global-set-key (kbd "M-;") 'my:move-line-down)
 
-(global-set-key (kbd "C-'") 'my:push-mark-no-activate)
-(global-set-key (kbd "M-'") 'my:jump-to-mark)
+(global-set-key (kbd "C-x m") 'my:push-mark-no-activate)
+(global-set-key (kbd "C-x p") 'pop-to-mark-command)
 
-(global-set-key (kbd "C-x C-;") 'comment-or-uncomment-region)
-(global-set-key (kbd "C-<backspace>") 'my:kill-line-to-indent)
 (global-set-key (kbd "C-x C-k") 'my:kill-and-close-current)
+(global-set-key (kbd "C-x C-;") 'comment-or-uncomment-region)
+
+(global-set-key (kbd "C-<backspace>") 'my:kill-line-to-indent)
+(global-set-key (kbd "C-<delete>") 'kill-line)
+(global-set-key (kbd "M-<backspace>") 'backward-kill-word)
+(global-set-key (kbd "M-<delete>") 'kill-word)
+
 
 ;; ### PACKAGES ### ;;
 (defun my:package-initialize ()
   ;; Navigation, editing, appearance
   (use-package linum-relative
     :ensure t
-    :config (progn (setq-default linum-relative-format "%4s ")))
+    :init (progn (setq-default linum-relative-format "%4s ")))
   (use-package undo-tree
     :ensure t
-    :config (global-undo-tree-mode))
-  (use-package smart-mode-line
-    :ensure t
-    :disabled t
-    :config (progn (sml/setup)))
+    :config (progn
+            (global-undo-tree-mode)
+            (setq undo-tree-visualizer-timestamps t
+                  undo-tree-visualizer-diff t)))
   (use-package powerline
     :ensure t
-    :config (progn
-              (powerline-center-evil-theme)))
+    :config (powerline-center-evil-theme))
   (use-package buffer-move
     :ensure t
     :config (progn
@@ -264,8 +285,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (use-package expand-region
     :ensure t
     :config (progn
-              (global-set-key (kbd "C-t") 'er/expand-region)
-              (global-set-key (kbd "C-S-t") 'er/contract-region)))
+              (global-set-key (kbd "C-=") 'er/expand-region)))
   (use-package smartparens
     :ensure t
     :config (progn
@@ -285,13 +305,44 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (use-package yasnippet
     :ensure t
     :idle (yas-global-mode t)
-    :config (progn 
+    :config (progn
               (add-to-list 'yas-snippet-dirs my:snippets-dir)
               (define-key yas-minor-mode-map (kbd "<tab>") nil)
               (define-key yas-minor-mode-map (kbd "TAB") nil)
               (add-to-list 'hippie-expand-try-functions-list
                            'yas-hippie-try-expand)))
   ;; Fast access and searching
+  (use-package helm-config
+    :ensure helm
+    :config (progn
+              (helm-mode 1)
+              (defun my:helm-completion (engine actions)
+                (mapc
+                 (lambda (action)
+                   (add-to-list 'helm-completing-read-handlers-alist `(,action . ,engine)))
+                 actions))
+              (my:helm-completion nil '(switch-to-buffer kill-buffer))
+              (set-face-attribute 'helm-selection nil :underline nil)
+              (set-face-attribute 'helm-selection-line nil :underline nil)
+              (set-face-attribute 'helm-ff-directory nil :background nil)
+              (my:kmap "t" 'helm-etags-select)
+              (my:kmap "i" 'helm-semantic-or-imenu)
+              (my:kmap "e" 'helm-list-emacs-process)
+              (my:kmap "r" 'helm-resume)
+              (global-set-key (kbd "C-x b") 'helm-mini)
+              (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+              (global-set-key (kbd "C-x C-f") 'helm-find-files)
+              (global-set-key (kbd "M-x") 'helm-M-x)
+              (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+              (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+              (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+              (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+              (define-key helm-map (kbd "C-z") 'helm-select-action)))
+  (use-package helm-swoop
+    :ensure t)
+  (use-package ido-vertical-mode
+    :ensure t
+    :config (ido-vertical-mode 1))
   (use-package flx-ido
     :ensure t
     :config (progn
@@ -299,13 +350,12 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
               (setq ido-use-faces nil)))
   (use-package ido-ubiquitous
     :ensure t
+    :disabled t
     :config (ido-ubiquitous))
-  (use-package ido-vertical-mode
-    :ensure t
-    :config (ido-vertical-mode 1))
   (use-package smex
     :ensure t
-    :config (progn 
+    :disabled t
+    :config (progn
               (global-set-key (kbd "M-x") 'smex)
               (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
   (use-package ag
@@ -313,16 +363,18 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     :config (progn (setq ag-highlight-search t)))
   ;; External tools
   (use-package magit
+    :defer t
     :ensure t)
   ;; Project management and project tree
   (use-package neotree
     :ensure t
     :init (progn
+            (setq neo-persist-show nil)
             (global-set-key (kbd "<f5>") 'neotree-toggle)
             (global-set-key (kbd "<f6>") 'neotree-find)
-            (define-key neotree-mode-map (kbd "r") 'neotree-refresh) 
-            (define-key neotree-mode-map (kbd "h") 'neotree-hidden-file-toggle) 
-            (define-key neotree-mode-map (kbd "a") 'neotree-stretch-toggle) 
+            (define-key neotree-mode-map (kbd "r") 'neotree-refresh)
+            (define-key neotree-mode-map (kbd "h") 'neotree-hidden-file-toggle)
+            (define-key neotree-mode-map (kbd "a") 'neotree-stretch-toggle)
             (define-key neotree-mode-map (kbd "p") 'neotree-previous-node)
             (define-key neotree-mode-map (kbd "n") 'neotree-next-node)
             (define-key neotree-mode-map (kbd "k") 'neotree-previous-node)
@@ -346,6 +398,10 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
                     (setq ad-return-value my:project-root)
                   ad-do-it))
               (projectile-global-mode)))
+  (use-package helm-projectile
+    :ensure t
+    :config (progn
+              (my:kmap "p" 'helm-projectile)))
   ;; Evil mode and Co
   (use-package evil
     :ensure t
