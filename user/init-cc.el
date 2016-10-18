79e12d6efb3874baead6ce76696a157a030915e6
Andrey Bushev <hlam-box@yandex.ru>
2016-10-17 23:38:33 -0400

    Tweaks for irony-mode on windows

--- user/init-cc.el
+++ user/init-cc.el
@@ -28,12 +28,19 @@
                   (other . "gnu"))))
 
 
+(when (string= system-type "windows-nt")
+  (defun my:-w32-lowercase-path (proc &rest args)
+    "Bind `w32-downcase-file-names' and call PROC with ARGS."
+    (let ((w32-downcase-file-names t))
+      (apply proc args))))
+
+
 (when (executable-find "clang")
   (my:with-package irony
     :ensure t
-    :init (progn
-            (add-hook 'c++-mode-hook 'irony-mode)
-            (add-hook 'c-mode-hook 'irony-mode)))
+    :config (when (fboundp 'my:-w32-lowercase-path)
+              (advice-add 'irony-cdb-json--adjust-compile-options :around
+                          #'my:-w32-lowercase-path)))
 
   (my:with-package company-irony
     :ensure t
@@ -45,13 +52,14 @@
             (flycheck-irony-setup)))
 
   (my:with-package irony-eldoc
+    :disabled t
     :ensure t
     :init (add-hook 'irony-mode-hook 'irony-eldoc)))
 
 
 (my:with-package company-c-headers
   :ensure t
-  :config (add-to-list 'company-backends #'company-c-headers))
+  :init (add-to-list 'company-backends #'company-c-headers))
 
 
 (my:with-package ggtags
