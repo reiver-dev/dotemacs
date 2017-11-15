ad73d7151fb78ccd7a2828adef72fe8d88a70769
Andrey Bushev <hlam-box@yandex.ru>
2017-11-15 01:08:52 -0500

    More org mode customizations
    - unfolded by default
    - sub-script with braces only

--- user/init-org.el
+++ user/init-org.el
@@ -1,33 +1,48 @@
 ;;; init-org.el --- Org-Mode configuration -*- lexical-binding: t -*-
 
 ;;; Commentary:
+;;
+;; This files provides customizations and
+;; function used in org-mode
 
 ;;; Code:
 
 (require 'init-package)
+(require 'init-list)
 
 
 (setq-default org-src-fontify-natively t
               org-src-tab-acts-natively t
               org-src-window-setup 'current-window
+              org-startup-folded nil
+              org-use-sub-superscripts (quote {})
               ;; set maximum indentation for description lists
               org-list-description-max-indent 5
               ;; prevent demoting heading also shifting text inside sections
               org-adapt-indentation nil)
 
 
+(defun -my:org-add-language (&rest langs)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     (append (my:remove-if (lambda (l) (memq (car l) langs))
+                           org-babel-load-languages)
+             (mapcar (lambda (l) (cons l t)) langs))))
+
+
 (my:after 'org
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((python . t))))
+  (-my:org-add-language 'python 'plantuml))
+
+
+(my:after 'plantuml-mode
+  (add-to-list 'org-src-lang-modes '("plantuml" . plantuml)))
 
 
 (my:with-package ob-ipython
+  :if (executable-find "jupyter")
   :ensure t
   :init (my:after 'org
-          (org-babel-do-load-languages
-           'org-babel-load-languages
-           '((ipython . t)))))
+          (-my:org-add-language 'ipython)))
 
 
 (provide 'init-org)
