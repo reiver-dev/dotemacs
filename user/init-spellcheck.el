f45a9f469fb59ee85210167caf9071687253cd28
Andrey Bushev <hlam-box@yandex.ru>
2018-04-16 02:02:10 -0400

    Disable automatic ispell configuration, add dictionary definition helpers

--- user/init-spellcheck.el
+++ user/init-spellcheck.el
@@ -9,6 +9,39 @@
 
 (defconst -my:hunspell-path (executable-find "hunspell"))
 
+
+(defun my:spell-dictionary (name &rest params)
+  (let* ((word (plist-get params :word))
+         (non-word (plist-get params :non-word))
+         (other (plist-get params :other))
+         (many-other (plist-get params :many-other?))
+         (args (plist-get params :args))
+         (extended-chars (plist-get params :extended-chars))
+         (encoding (or (plist-get params :encoding) 'utf-8)))
+    (list name word non-word other many-other args extended-chars encoding)))
+
+(put 'my:spell-dictionary 'lisp-indent-function 'defun)
+
+
+(defun my:spell-dictionary-ru (name dictname)
+  (my:spell-dictionary name
+    :word "[Ё-ё]"
+    :non-word "[^Ё-ё]"
+    :other "[-]"
+    :many-other? nil
+    :args (list "-d" dictname)
+    :encoding 'utf-8))
+
+
+(defun my:spell-dictionary-en (name dictname)
+  (my:spell-dictionary name
+    :word "[A-z]"
+    :non-word "[^A-z]"
+    :other "[']"
+    :args (list "-d" dictname)
+    :encoding 'iso-8859-1))
+
+
 (my:with-package ispell
   :if -my:hunspell-path
   :init (progn
@@ -16,29 +49,7 @@
                         flyspell-issue-message-flag nil)
           (setenv "DICPATH"
                   (expand-file-name "../share/hunspell"
-                                    (file-name-directory -my:hunspell-path)))
-          (setenv "DICTIONARY" "default"))
-  :config
-  (progn
-    (add-to-list 'ispell-local-dictionary-alist
-                 '("russian"
-                   "[Ё-ё]"  ;; Word characters
-                   "[^Ё-ё]" ;; Non-word characters
-                   "[-]"    ;; Non-word characters in words
-                   nil      ;; Many non-word chars?
-                   ("-d" "ru_RU") ;; Args to use this dictionary
-                   nil      ;; Ispell-related extenden char mode
-                   utf-8))  ;; Charset checker uses
-    (add-to-list 'ispell-local-dictionary-alist
-                 '("english"
-                   "[A-z]"
-                   "[^A-z]"
-                   "[']"
-                   nil
-                   ("-d" "en_US")
-                   nil
-                   iso-8859-1))
-    (ispell-set-spellchecker-params)))
+                                    (file-name-directory -my:hunspell-path)))))
 
 (provide 'init-spellcheck)
 
