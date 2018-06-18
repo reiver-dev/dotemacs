102eb59f1d9c1ee7c0b75ed63e01399b43a0ec47
Andrey Bushev <hlam-box@yandex.ru>
2018-06-18 09:39:49 -0400

    Add change-theme that disables other themes before loading

--- user/init-theme.el
+++ user/init-theme.el
@@ -6,6 +6,19 @@
 
 (deftheme my:theme "My face theme settings")
 
+
+(defun my:change-theme (&rest args)
+  "Like `load-theme', but disables all themes before loading the new one."
+  ;; The `interactive' magic is for creating a future-proof passthrough.
+  (interactive (advice-eval-interactive-spec
+                (cadr (interactive-form #'load-theme))))
+  (dolist (theme custom-enabled-themes)
+    (unless (eq theme 'my:theme)
+      (disable-theme theme)))
+  (apply (if (called-interactively-p 'any) #'funcall-interactively #'funcall)
+         #'load-theme args))
+
+
 (custom-theme-set-faces
  'my:theme
 
