49aacbe429c73b106d2cfe0078e20895c0ee0792
Andrey Bushev <hlam-box@yandex.ru>
2019-02-09 17:38:35 -0500

    Formatting

--- user/init-compile.el
+++ user/init-compile.el
@@ -14,18 +14,21 @@
  compilation-scroll-output 'next-error ;; stop on error
  compilation-skip-threshold 2)         ;; skip warnings
 
+
 (defun my:compile (&optional comint)
   "Compile without confirmation.
 If used with a COMINT argument, use `comint-mode'."
-      (interactive "P")
-      ;; Do the command without a prompt.
-      (save-window-excursion
-        (compile (eval compile-command) (and comint t)))
-      ;; Create a compile window of the desired width.
-      (pop-to-buffer (get-buffer "*compilation*"))
-      (enlarge-window (- (frame-width) 105 (window-width)) 'horizontal))
+  (interactive "P")
+  ;; Do the command without a prompt.
+  (save-window-excursion
+    (compile (eval compile-command) (and comint t)))
+  ;; Create a compile window of the desired width.
+  (pop-to-buffer (get-buffer "*compilation*"))
+  (enlarge-window (- (frame-width) 105 (window-width)) 'horizontal))
+
 
 (autoload 'my:compile "compile")
+
 
 (my:kmap* prog-mode-map
           ("<f8>" #'my:compile)
