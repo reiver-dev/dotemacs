e4fa0c6eed1a001519b99ff65a81559ec32a7787
Andrey Bushev <hlam-box@yandex.ru>
2019-03-06 20:37:12 -0500

    Formatting

--- user/init-flymake.el
+++ user/init-flymake.el
@@ -11,7 +11,8 @@
   "Fix disabling the backend if region range is (nil . nil).
 Set diagnostic region as last character of the buffer. DIAGNOSTIC is
 `flymake--diag' struct."
-  (when (not (and (flymake--diag-beg diagnostic) (flymake--diag-end diagnostic)))
+  (when (not (and (flymake--diag-beg diagnostic)
+                  (flymake--diag-end diagnostic)))
     (let* ((buffer (flymake--diag-buffer diagnostic))
            (end (with-current-buffer buffer (point-max)))
            (begin (max (1- end) 1)))
