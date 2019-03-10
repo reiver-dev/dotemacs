f6b60ed82946b4df879c361a7545f41f6d285c1b
Andrey Bushev <hlam-box@yandex.ru>
2019-03-10 15:54:42 -0400

    Remove commented rust racer configuration

--- user/init-rust.el
+++ user/init-rust.el
@@ -10,30 +10,17 @@
 
 (defconst -my:rust-has-racer (executable-find "racer"))
 (defconst -my:rust-has-cargo (executable-find "cargo"))
+(defconst -my:rust-has-rls (executable-find "rls"))
+
 
 (my:with-package rust-mode
   :ensure t
   :init (setq-default rust-indent-where-clause t))
 
-;; (my:with-package racer
-;;   :if -my:rust-has-racer
-;;   :ensure t)
-
-;; (my:with-package company-racer
-;;   :if -my:rust-has-racer
-;;   :ensure t
-;;   :init (my:after (company racer)
-;;           (add-to-list 'company-backends #'company-racer)))
-
-;; (my:with-package flycheck-rust
-;;   :if -my:rust-has-racer
-;;   :ensure t)
-
 (my:with-package lsp-rust
   :ensure t
   :init (my:after rust-mode (require 'lsp-rust))
   :config (add-hook 'rust-mode-hook #'lsp-rust-enable))
-
 
 
 (my:with-package cargo
