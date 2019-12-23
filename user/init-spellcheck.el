9f949fb62e983f7ee06b579971df137b5d8d5453
Andrey Bushev <hlam-box@yandex.ru>
2019-12-23 17:49:39 -0500

    Add documentation for specheck dictionaly functions

--- user/init-spellcheck.el
+++ user/init-spellcheck.el
@@ -10,20 +10,25 @@
 (defconst -my:hunspell-path (executable-find "hunspell"))
 
 
-(defun my:spell-dictionary (name &rest params)
-  (let* ((word (plist-get params :word))
-         (non-word (plist-get params :non-word))
-         (other (plist-get params :other))
-         (many-other (plist-get params :many-other?))
-         (args (plist-get params :args))
-         (extended-chars (plist-get params :extended-chars))
-         (encoding (or (plist-get params :encoding) 'utf-8)))
+(defun my:spell-dictionary (name &rest parameters)
+  "Construct `ispell' dictionary NAME and PARAMETERS.
+The result is a list that can be a possible member of
+`ispell-dictionary-alist'."
+  (let* ((word (plist-get parameters :word))
+         (non-word (plist-get parameters :non-word))
+         (other (plist-get parameters :other))
+         (many-other (plist-get parameters :many-other?))
+         (args (plist-get parameters :args))
+         (extended-chars (plist-get parameters :extended-chars))
+         (encoding (or (plist-get parameters :encoding) 'utf-8)))
     (list name word non-word other many-other args extended-chars encoding)))
 
 (put 'my:spell-dictionary 'lisp-indent-function 'defun)
 
 
 (defun my:spell-dictionary-ru (name dictname)
+  "Make dictionary configuration for Russian.
+NAME is a name identifier and DICTNAME is filename."
   (my:spell-dictionary name
     :word "[Ё-ё]"
     :non-word "[^Ё-ё]"
@@ -34,6 +39,8 @@
 
 
 (defun my:spell-dictionary-en (name dictname)
+  "Make dictionary configuration for English.
+NAME is a name identifier and DICTNAME is filename."
   (my:spell-dictionary name
     :word "[A-z]"
     :non-word "[^A-z]"
