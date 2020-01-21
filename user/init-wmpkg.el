f412c8aa1f97cf418f8c6ca11e1c86bd573f1980
Andrey Bushev <hlam-box@yandex.ru>
2020-01-20 23:41:16 -0500

    Switch to winum package from ace-window

--- user/init-wmpkg.el
+++ user/init-wmpkg.el
@@ -7,6 +7,7 @@
 (require 'init-package)
 (require 'init-keybind)
 (require 'init-wm)
+(require 'init-chars)
 
 
 (my:with-package shackle
@@ -18,7 +19,67 @@
                  '(" *NeoTree*" :align left :size 25))))
 
 
+(my:with-package winum
+  :ensure t
+  :init
+  (progn
+    (setq-default winum-auto-assign-0-to-minibuffer t)
+    (winum-set-keymap-prefix nil)
+    (winum-mode +1))
+  :config
+  (progn
+    (defun my:winum-action (num func &optional current)
+      (let ((win (winum-get-window-by-number (abs num))))
+        (if win
+            (if current
+                (my:apply-to-window func win (selected-window))
+              (my:apply-to-window func win))
+          (error "No window numbered %d" num))))
+    (defun my:winum-update ()
+      (winum--update)
+      (force-mode-line-update t))
+    (defun my:winum-dispatch ()
+      (interactive)
+      (let ((action #'select-window)
+            (action-name "Select")
+            current
+            c exit)
+        (while (not exit)
+          (setq c (read-char (format "Window action: %s" action-name)))
+          (cond
+           ((eq c ?0) (winum-select-window-0) (setq exit t))
+           ((my:char-digit-p c)
+            (my:winum-action (- c ?0) action current)
+            (setq exit t))
+           ((my:any-of c ?h ?\C-h) (windmove-left) (setq exit t))
+           ((my:any-of c ?j ?\C-j) (windmove-down) (setq exit t))
+           ((my:any-of c ?k ?\C-k) (windmove-up) (setq exit t))
+           ((my:any-of c ?l ?\C-l) (windmove-right) (setq exit t))
+           ((eq c ?m) (setq action #'my:query-move-to-window
+                            action-name "Move"
+                            current t))
+           ((eq c ?s) (setq action #'my:swap-windows
+                            action-name "Swap"
+                            current t))
+           ((eq c ?x) (setq action #'my:delete-window
+                            action-name "Delete"
+                            current nil))
+           ((eq c ?n) (my:detach-window) (setq exit t))
+           ((eq c ?r) (my:resize-window) (setq exit t))
+           ((eq c ?u) (my:winum-update) (setq exit 1))
+           ((my:char-exit-p c)
+            (setq exit t))))))
+    (defun -my:winum--window-list ()
+      "Return a list of interesting windows."
+      (my:mapcan #'my:window-list
+                 (sort (my:visible-frame-list) #'my:frame-sort-predicate)))
+    (fset 'winum--window-list '-my:winum--window-list)
+    (my:kmap
+     ("C-c w" "C-c C-w" #'my:winum-dispatch))))
+
+
 (my:with-package ace-window
+  :disabled t
   :ensure t
   :init
   (progn
